8265babd11770d9a3c1dd92468da32963f3dab51
Supapittoon <6210110695@psu.ac.th>
2022-08-27 21:46:41 -0700

    part56

--- components/Weather.js
+++ components/Weather.js
@@ -1,28 +1,44 @@
-import React, { useState } from "react";
-import {Text, ImageBackground,StyleSheet,View}  from "react-native";
+import React, { useState , useEffect } from "react";
+import { Text, ImageBackground, StyleSheet, View } from "react-native";
 import Forecast from "./Forecast";
 
-
-
 export default function Weather(props) {
+  useEffect(() => {
+    console.log(`fetching data with zipCode = ${props.zipCode}`);
+    if (props.zipCode) {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=ba71be1f648c0f1670bfeba150347064
+        `
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          setForecastInfo({
+            main: json.weather[0].main,
+            description: json.weather[0].description,
+            temp: json.main.temp,
+          });
+        })
+        .catch((error) => {
+          console.warn(error);
+        });
+    }
+  }, [props.zipCode]);
   const [forecastInfo, setForecastInfo] = useState({
     main: "-",
     description: "-",
     temp: 0,
   });
   return (
-    
-      <ImageBackground source={require("../bg.jpg")} style={styles.backdrop}>
-        <Text>Zip Code</Text>
-        <Text>{props.zipCode}</Text>
-        <Forecast {...forecastInfo} />
-      </ImageBackground>
-   
+    <ImageBackground source={require("../bg.jpg")} style={styles.backdrop}>
+      <Text>Zip Code</Text>
+      <Text>{props.zipCode}</Text>
+      <Forecast {...forecastInfo} />
+    </ImageBackground>
   );
 }
 const styles = StyleSheet.create({
   backdrop: {
-    flexDirection: 'column',
+    flexDirection: "column",
     width: "100%",
     height: "100%",
   },
