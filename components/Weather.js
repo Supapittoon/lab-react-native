02e43364c461a1bc608af65e3cbbe2b664451e79
Supapittoon <6210110695@psu.ac.th>
2022-08-27 22:42:18 -0700

    add color

--- components/Weather.js
+++ components/Weather.js
@@ -1,51 +1,58 @@
-import React, { useState, useEffect } from "react";
-import { Text, ImageBackground, StyleSheet, View } from "react-native";
-import Forecast from "./Forecast";
+import React, { useState, useEffect } from 'react';
+import { ImageBackground, Text, View, StyleSheet } from 'react-native';
+import Forecast from './Forecast';
 
 export default function Weather(props) {
-  useEffect(() => {
-    console.log(`fetching data with zipCode = ${props.zipCode}`);
-    if (props.zipCode) {
-      fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=ba71be1f648c0f1670bfeba150347064
-        `
-      )
-        .then((response) => response.json())
-        .then((json) => {
-          setForecastInfo({
-            name: json.name,
-            main: json.weather[0].main,
-            temp: json.main.temp,
-            country: json.sys.country,
-            humidity: json.main.humidity,
-            pressure: json.main.pressure,
-          });
-        })
-        .catch((error) => {
-          console.warn(error);
-        });
-    }
-  }, [props.zipCode]);
-  const [forecastInfo, setForecastInfo] = useState({
-    name: 'N/A',
-    country: 'N/A',
-    main: 'N/A',
-    temp: 0,
-    humidity: 0,
-    pressure: 0,
-  });
-  return (
-    <ImageBackground source={require("../bg.jpg")} style={styles.backdrop}>
-      <Text>Zip Code</Text>
-      <Text>{props.zipCode}</Text>
-      <Forecast {...forecastInfo} />
-    </ImageBackground>
-  );
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=ba71be1f648c0f1670bfeba150347064`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        name: json.name,
+                        main: json.weather[0].main,
+                        temp: json.main.temp,
+                        country: json.sys.country,
+                        wind_speed: json.wind.speed,
+                        humidity: json.main.humidity,
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
+    
+    const [forecastInfo, setForecastInfo] = useState({
+        name: 'N/A',
+        country: 'N/A',
+        main: 'N/A',
+        temp: 0,
+        wind_speed: 0,
+        humidity: 0,
+    });
+
+    
+    return (
+        <View>
+            <ImageBackground  source={require('../bg.jpg')} style={styles.backdrop}>
+                <View style={styles.box}>
+                    <Text style={styles.zip_code_text}>Zip Code : {props.zipCode}</Text>
+                    <Forecast {...forecastInfo} />
+                </View>
+            </ImageBackground>
+        </View>
+    );
 }
 const styles = StyleSheet.create({
-  backdrop: {
-    flexDirection: "column",
-    width: "100%",
-    height: "100%",
-  },
+    backdrop: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        
+
+
+    }
 });
